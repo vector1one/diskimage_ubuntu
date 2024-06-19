7d1fc8a9abf482c953ee8915e31a27f2d997fe4d
vector1one <39304498+vector1one@users.noreply.github.com>
2024-06-19 15:07:04 -0400

    Update diskimage_ubuntu.sh

--- diskimage_ubuntu.sh
+++ diskimage_ubuntu.sh
@@ -3,14 +3,11 @@
 #+100%FREE /dev/ubuntu-vg/ubuntu-lv
 #test for Ubuntu
 
-
-
 #!/bin/bash
 echo "extending disk to match physical"
 sleep1
-printf "n\np\n3\n\n\nt\n3\n8e\nw\n"|sudo fdisk /dev/sda
-sudo partprobe -s
-sudo partx -a /dev/sda3
-sudo vgextend centos /dev/sda3
-sudo lvextend -q -r -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+pvresize /dev/sda3
+lvdisplay
+lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
+resize2fs /dev/ubuntu-vg/ubuntu-lv
 echo "nice disk"
